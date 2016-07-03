fb08769ab0ee2ed570727adb0e396250ad2a0b05
Akshit Gupta <akshitonline@gmail.com>
2016-07-03 13:18:57 +0530

    Added option menu and changed the title

--- app/src/main/java/com/sam_chordas/android/stockhawk/ui/DetailsActivity.java
+++ app/src/main/java/com/sam_chordas/android/stockhawk/ui/DetailsActivity.java
@@ -2,15 +2,32 @@ package com.sam_chordas.android.stockhawk.ui;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
 
 import com.sam_chordas.android.stockhawk.R;
 
-public class DetailsActivity extends Activity {
+public class DetailsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.stock_details, menu);
+        restoreActionBar();
+        return true;
+    }
+
+    public void restoreActionBar() {
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
+        actionBar.setDisplayShowTitleEnabled(true);
+        actionBar.setTitle(getTitle());
     }
 
 }
